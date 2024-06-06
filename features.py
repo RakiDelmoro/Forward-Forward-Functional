975f57f323bbb032bc62bc899605137690c700c0
Raki Delmoro <rakidelmoro@gmail.com>
2024-06-06 01:27:39 +0000

    Update

--- features.py
+++ features.py
@@ -1,9 +1,4 @@
-import torch
-from functools import partial
-
 RED = '\033[31m'
 GREEN = '\033[32m'
 RESET = '\033[0m'
 WHITE_UNDERLINE = "\033[4;37m"
-
-tensor = partial(torch.tensor, device="cuda")
